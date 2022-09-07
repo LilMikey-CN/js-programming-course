85f5ab4c33da06489ea3d4baad62ed19669ad7f0
James Black <31362189+LilMikey-CN@users.noreply.github.com>
2022-09-08 01:13:01 +0800

    More experiments

---
+++ test_with_jest/addFive.js
@@ -0,0 +1,12 @@
+function addFive(val) {
+    return val + 5;
+}
+
+module.exports = addFive;
+
+// Then in command line input: npm init -y , to init a package.json and 
+// return yes to every question
+
+// Then input: npm install --save-dev jest 
+// --save-dev means to save modules locally/in this project. 
+// Comparing to -g option which save modules globally.
